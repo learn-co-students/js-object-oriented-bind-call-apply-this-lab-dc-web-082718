8dccb908371af8dc3b45cd45cebe82ce40d5ef4b
jb5595 <jacobberman1995@gmail.com>
2018-10-14 11:25:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //Your code here
+function justInvoke(passedIn) {
+  return passedIn()
+}
+function setThisWithCall(returnsThisAndArgs, bob, number){
+  return returnsThisAndArgs.call(bob, number)
+}
+function setThisWithApply(returnsThisAndArgs, bob, args) {
+  return returnsThisAndArgs.apply(bob,args )
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  return functionToBeCopied.bind(thisValue)
+}
