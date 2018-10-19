20c63e6d5201bc87f42aa55b39c94e5117b68da2
drubaby <peryton6@gmail.com>
2018-10-19 14:33:43 -0400

    Done.

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ function justInvoke(fn){
   return fn()
 }
 
+// invokes function passed to it but changes function's .this value, passes new arg to function
 function setThisWithCall(fn, thisValue, arg){
   return fn.call(thisValue, arg)
 }
 
+//same as above
 function setThisWithApply(fn, thisValue, arg) {
   return fn.apply(thisValue, arg)
 }
-
+//returns a copy of the function passsed through but sets the .this value of the functions copy
 function returnNewFunctionOf(functionToBeCopied, thisValue) {
   return functionToBeCopied.bind(thisValue)
 }
